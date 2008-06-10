6255fceed07a3ee7ee37a29cd6d3a4c54a0ba3e3
Aliaksey Kandratsenka <alekseyk@nettoo.com>
2008-06-10 21:40:28 +0300

    further development

--- main.c
+++ main.c
@@ -5,8 +5,54 @@
 #include <glade/glade.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <memory.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <time.h>
+#include "scorer.h"
+
+__attribute__((noreturn))
+static
+void memory_exhausted(void)
+{
+	fputs("memory exhausted", stderr);
+	abort();
+}
+
+struct vector {
+	char *buffer;
+	int eltsize;
+	int used;
+	int avail;
+};
+
+void *vector_append(struct vector *v)
+{
+	if (v->used == v->avail) {
+		int new_avail = v->avail * 2;
+		if (!new_avail)
+			new_avail = 4096 / v->eltsize;
+		v->buffer = realloc(v->buffer, new_avail * v->eltsize);
+		v->avail = new_avail;
+	}
+
+	return v->buffer + v->eltsize * (v->used++);
+}
+
+void vector_clear(struct vector *v)
+{
+	v->used = 0;
+	v->buffer = realloc(v->buffer, 0);
+	v->avail = 0;
+}
+
+static
+void finish_timing(clock_t start, char *info)
+{
+	clock_t ticks = clock() - start;
+	double msecs = ticks/(double)CLOCKS_PER_SEC*1000.0;
+	printf("%s took %g msecs\n", info, msecs);
+}
 
 struct filename {
 	char *p;
@@ -19,34 +65,16 @@ static GtkEntry *name_entry;
 static GtkTreeView *tree_view;
 static GtkListStore *list_store;
 
-static int files_avail;
-static int nfiles;
-static struct filename *files;
+struct vector files_vector = {.eltsize = sizeof(struct filename)};
 
-__attribute__((noreturn))
-static
-void memory_exhausted(void)
-{
-	fputs("memory exhausted", stderr);
-	abort();
-}
+#define nfiles (files_vector.used)
+#define files ((struct filename *)(files_vector.buffer))
 
 static
 void add_filename(char *p, int dirlength)
 {
-	struct filename *last;
+	struct filename *last = vector_append(&files_vector);
 
-	if (nfiles == files_avail) {
-		int new_avail = files_avail * 2;
-		if (!new_avail)
-			new_avail = 512;
-		files = realloc(files, sizeof(struct filename) * new_avail);
-		if (!files)
-			memory_exhausted();
-		files_avail = new_avail;
-	}
-
-	last = files + nfiles++;
 	last->p = p;
 	last->dirlength = dirlength;
 }
@@ -92,10 +120,76 @@ char *input_names(int fd, char **endp)
 	return buf;
 }
 
-static
-int filename_compare(const struct filename *a, const struct filename *b)
+struct filter_result {
+	int index;
+	int score;
+};
+
+int filter_filename(struct filename *name, char *pattern, struct filter_result *result)
 {
-	return strcmp(a->p, b->p);
+	int score = score_simple_string(name->p, pattern, 0);
+	if (score > 0) {
+		result->score = score;
+		return 1;
+	}
+	return 0;
+}
+
+struct vector filtered = {.eltsize = sizeof(struct filter_result)};
+
+static
+void filter_files(char *pattern)
+{
+	int i;
+	struct filter_result result;
+	struct filter_result *results;
+	GtkTreeIter iter;
+	clock_t start;
+
+	vector_clear(&filtered);
+
+	for (i=0; i<nfiles; i++) {
+		int passes = filter_filename(files + i, pattern, &result);
+		struct filter_result *place;
+		if (!passes)
+			continue;
+		place = vector_append(&filtered);
+		result.index = i;
+		*place = result;
+	}
+
+	results = (struct filter_result *)filtered.buffer;
+
+	start = clock();
+
+	g_object_ref(list_store);
+	gtk_tree_view_set_model(tree_view, 0);
+
+	gtk_list_store_clear(list_store);
+
+	finish_timing(start, "gtk_list_store_clear");
+	start = clock();
+
+	for (i=0; i<filtered.used; i++) {
+		gtk_list_store_append(list_store, &iter);
+		gtk_list_store_set(list_store, &iter,
+				   0, results[i].index,
+				   -1);
+	}
+
+	finish_timing(start, "adding filtered data");
+	start = clock();
+
+	gtk_tree_view_set_model(tree_view, GTK_TREE_MODEL(list_store));
+	g_object_unref(list_store);
+
+	finish_timing(start, "setting model back");
+}
+
+static
+int filename_compare(struct filename *a, struct filename *b)
+{
+	return strcasecmp(a->p, b->p);
 }
 
 static
@@ -126,14 +220,15 @@ void cell_data_func(GtkTreeViewColumn *col,
 		    gpointer dummy)
 {
 	GtkCellRendererText *renderer = GTK_CELL_RENDERER_TEXT(_renderer);
+	int index;
 	char *text=0;
-	gtk_tree_model_get(model, iter, 0, &text, -1);
+	gtk_tree_model_get(model, iter, 0, &index, -1);
+	text = files[index].p;
 	if (text) {
 		g_object_set(G_OBJECT(renderer),
 			     "text", text,
-			     0);
+			     NULL);
 	}
-	free(text);
 }
 
 static
@@ -144,7 +239,7 @@ void setup_column(void)
 	g_object_set(G_OBJECT(renderer),
 		     "ellipsize", PANGO_ELLIPSIZE_START,
 		     "alignment", PANGO_ALIGN_RIGHT,
-		     0);
+		     NULL);
 
 	col = gtk_tree_view_column_new_with_attributes("filename", renderer, NULL);
 
@@ -157,11 +252,11 @@ void setup_column(void)
 static
 void setup_data(void)
 {
-	list_store = gtk_list_store_new(1, G_TYPE_STRING);
+	list_store = gtk_list_store_new(1, G_TYPE_INT);
 	gtk_tree_view_set_model(tree_view, GTK_TREE_MODEL(list_store));
 
 	{
-		FILE *pipe = popen("find /root/src/altoros/phase1 -type f -print0","r");
+		FILE *pipe = popen("(cd /root/src/altoros/phase1; find -type f -print0)","r");
 		read_filenames(fileno(pipe));
 		fclose(pipe);
 	}
@@ -172,7 +267,7 @@ void setup_data(void)
 		for (i=0;i<nfiles;i++) {
 			gtk_list_store_append(list_store, &iter);
 			gtk_list_store_set(list_store, &iter,
-					   0, files[i].p,
+					   0, i,
 					   -1);
 		}
 	}
@@ -206,30 +301,61 @@ gboolean on_top_window_keypress(GtkWidget *_dummy,
 }
 
 static
+void on_entry_changed(GtkEditable *editable,
+		      gpointer data)
+{
+	clock_t start = clock();
+
+	char *text = g_strdup(gtk_entry_get_text(GTK_ENTRY(editable)));
+	filter_files(text);
+	free(text);
+
+	finish_timing(start, "filtration");
+}
+
+static
 void setup_signals(void)
 {
 	g_signal_connect(top_window, "destroy", G_CALLBACK(exit_program), 0);
 	g_signal_connect(top_window, "key-press-event", G_CALLBACK(on_top_window_keypress), 0);
 	g_signal_connect(name_entry, "activate", G_CALLBACK(choice_made), 0);
 	g_signal_connect(tree_view, "row-activated", G_CALLBACK(choice_made), 0);
+	g_signal_connect(name_entry, "changed", G_CALLBACK(on_entry_changed), 0);
 }
 
 int main(int argc, char **argv)
 {
+	clock_t tstart = clock();
+
 	gtk_init(&argc, &argv);
 	glade_ui = glade_xml_new("filechooser.glade", 0, 0);
+
+	finish_timing(tstart, "gtk initialization");
+
+	tstart = clock();
 
 	top_window = GTK_WINDOW(glade_xml_get_widget(glade_ui, "top-window"));
 	tree_view = GTK_TREE_VIEW(glade_xml_get_widget(glade_ui, "treeview"));
 	name_entry = GTK_ENTRY(glade_xml_get_widget(glade_ui, "name-entry"));
 
+	finish_timing(tstart, "init2");
+	tstart = clock();
+
 	gtk_widget_show_all(GTK_WIDGET(top_window));
 	while (gtk_events_pending())
 		gtk_main_iteration();
 
+	finish_timing(tstart, "initial show");
+	tstart = clock();
+
 	setup_signals();
 
+	finish_timing(tstart, "setup_signals");
+	tstart = clock();
+
 	setup_data();
+
+	finish_timing(tstart, "setup_data");
 
 	gtk_main();
 	return 0;
