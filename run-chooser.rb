ff8848d046acabb53f905432307b7f8d32ba22a8
Aliaksey Kandratsenka <alekseyk@nettoo.com>
2008-06-20 15:47:39 +0300

    small emacs integration fix

--- run-chooser.rb
+++ run-chooser.rb
@@ -3,4 +3,12 @@
 path = ARGV[0] || raise('give me path')
 chooser = File.join(File.dirname(__FILE__), 'filechooser')
 
-exec "(cd #{path}; find '!' -wholename '*.git/*' -a '!' -wholename '*.hg/*' -a '!' -wholename '*.svn/*' -type f -print0) | (cd #{File.dirname(__FILE__)}; #{chooser})"
+cmdline = "(cd #{path}; find '!' -wholename '*.git/*' -a '!' -wholename '*.hg/*' -a '!' -wholename '*.svn/*' -type f -print0) | (cd #{File.dirname(__FILE__)}; #{chooser})"
+
+answer = `#{cmdline}`
+answer = eval(answer)
+if answer
+  answer = File.join(path, answer)
+end
+
+puts answer.inspect
