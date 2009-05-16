2694959f9cc9b5cb9382959f4b01fdbd5c573d80
Aliaksey Kandratsenka <alk@tut.by>
2009-05-16 20:47:03 +0300

    indentation fix

--- main.c
+++ main.c
@@ -84,10 +84,10 @@ char *input_names(int fd, char **endp)
 		}
 	} while (1);
 	if (filled) {
-			if (buf[filled-1])
-					filled++;
-			buf = xrealloc(buf, filled);
-			buf[filled-1] = 0;
+		if (buf[filled-1])
+			filled++;
+		buf = xrealloc(buf, filled);
+		buf[filled-1] = 0;
 	}
 	*endp = buf+filled;
 	return buf;
